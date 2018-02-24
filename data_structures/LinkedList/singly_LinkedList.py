1ae9a85b336d08ffa949fe334745573890fd56a8
bunengzhucefeng <36393781+bunengzhucefeng@users.noreply.github.com>
2018-02-25 01:26:25 +0800

    Update singly_LinkedList.py

--- data_structures/LinkedList/singly_LinkedList.py
+++ data_structures/LinkedList/singly_LinkedList.py
@@ -1,69 +1,25 @@
-from __future__ import print_function
-
-
 class Node:  # create a Node
-    def __int__(self, data):
+    def __init__(self, data, nextnode=None):
         self.data = data  # given data
-        self.next = None  # given next to None
-
-
+        self.nextnode = nextnode  # given nextnode to None
+        
 class Linked_List:
-    def insert_tail(Head, data):
-        if Head.next is None:
-            Head.next = Node(data)
-        else:
-            Head.next.insert_tail(data)
+    def __init__(self,header=Node(None)):
+        self.header=header
+                 
+    def insert_tail(self, tmpnode):
+        tmp=self.header
+        while not(tmp.nextnode==None):
+            tmp=tmp.nextnode
+        tmp.nextnode=tmpnode
 
-    def insert_head(Head, data):
-        tamp = Head
-        if tamp is None:
-            newNod = Node()  # create a new Node
-            newNod.data = data
-            newNod.next = None
-            Head = newNod  # make new node to Head
-        else:
-            newNod = Node()
-            newNod.data = data
-            newNod.next = Head  # put the Head at NewNode Next
-            Head = newNod  # make a NewNode to Head
-        return Head
+    def insert_afterheader(self, tmpnode):
+        tmp=self.header.nextnode
+        self.header.nextnode=tmpnode
+        tmpnode.nextnode=tmp
 
-    def printList(Head):  # print every node data
-        tamp = Head
-        while tamp is not None:
-            print(tamp.data)
-            tamp = tamp.next
-
-    def delete_head(Head):  # delete from head
-        if Head is not None:
-            Head = Head.next
-        return Head  # return new Head
-
-    def delete_tail(Head):  # delete from tail
-        if Head is not None:
-            tamp = Node()
-            tamp = Head
-            while tamp.next.next is not None:  # find the 2nd last element
-                tamp = tamp.next
-            # delete the last element by give next None to 2nd last Element
-            tamp.next = None
-        return Head
-
-    def isEmpty(Head):
-        return Head is None  # Return if Head is none
-
-    def reverse(Head):
-        prev = None
-        current = Head
-
-        while current:
-            # Store the current node's next node.
-            next_node = current.next
-            # Make the current node's next point backwards
-            current.next = prev
-            # Make the previous node be the current node
-            prev = current
-            # Make the current node the next node (to progress iteration)
-            current = next_node
-        # Return prev in order to put the head at the end
-        Head = prev
+    def printList(self):  # print every node data
+        tmp = self.header.nextnode
+        while tmp is not None:
+            print(tmp.data)
+            tmp = tmp.nextnode
